482027f7319ec4f3057099486ec54b6a8e2d9829
PipitoneSamuele <samuele.pipitone@gmail.com>
2023-09-01 11:10:37 +0200

    better graphics

--- Ecommerce-client/src/app/products/products.component.ts
+++ Ecommerce-client/src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,16 +6,24 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.sass']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnChanges {
 
   products: any;
+  counter: number;
 
   constructor(private httpClient: HttpClient) { }
+
+
+  ngOnChanges(): void {
+    this.counter = this.counter + 1;
+    console.log(this.counter);
+  }
 
   ngOnInit(): void {
     this.httpClient.get<any>("http://localhost:8080/products").subscribe(data => {
       this.products = data;
     });
   }
+ 
 
 }
