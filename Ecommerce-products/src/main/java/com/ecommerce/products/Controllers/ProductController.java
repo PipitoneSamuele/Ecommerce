536a8c2477d911a53d9bc798e8c44821c6c178c3
PipitoneSamuele <samuele.pipitone@gmail.com>
2023-09-02 12:31:54 +0200

    add mongoDB driver and extraction of products from db

--- Ecommerce-products/src/main/java/com/ecommerce/products/Controllers/ProductController.java
+++ Ecommerce-products/src/main/java/com/ecommerce/products/Controllers/ProductController.java
@@ -1,16 +1,22 @@
 package com.ecommerce.products.Controllers;
 
 import com.ecommerce.products.entities.Product;
-import com.ecommerce.products.staticResources.MockProducts;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.*;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ProductController {
+
+    CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+            CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
     @GetMapping("/")
     public String home() {
@@ -19,7 +25,21 @@ public class ProductController {
 
     @GetMapping("/products")
     public List<Product> getProducts() {
-        return MockProducts.getMockProducts();
+        List<Product> products = new ArrayList<>();
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            MongoDatabase db = mongoClient.getDatabase("ecommerce").withCodecRegistry(pojoCodecRegistry);
+            MongoCollection<Product> productsCollection = db.getCollection("products", Product.class);
+
+            FindIterable<Product> productsIterable = productsCollection.find();
+            MongoCursor<Product> cursor = productsIterable.cursor();
+            while(cursor.hasNext()) {
+                Product p = (Product) cursor.next();
+                products.add(p);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 
 }
---
+++ Ecommerce-products/src/main/java/com/ecommerce/products/db/DbTesting.java
@@ -0,0 +1,33 @@
+package com.ecommerce.products.db;
+
+import com.ecommerce.products.entities.Product;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.*;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+public class DbTesting {
+
+    public static void main(String[] args) {
+
+        CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            MongoDatabase db = mongoClient.getDatabase("ecommerce").withCodecRegistry(pojoCodecRegistry);
+            MongoCollection<Product> productsCollection = db.getCollection("products", Product.class);
+
+            FindIterable<Product> products = productsCollection.find();
+            MongoCursor cursor = products.cursor();
+            while(cursor.hasNext()) {
+                Product p = (Product) cursor.next();
+                System.out.println(p.getAmount() + " " + p.getDescription());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
